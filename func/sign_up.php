3d9d35a7ab1149b75b41e09ec18e7e92bd507229
daniel lim <daniellim4400@gmail.com>
2020-05-14 20:15:32 +0700

    update

--- func/sign_up.php
+++ func/sign_up.php
@@ -7,20 +7,14 @@
     $alamat = $_POST['alamat'];
     $email = $_POST['email'];
 
-    // $insert = mysqli_query($koneksi, "insert into member 
-    // values ('', '$username', '$password', '$nama_lengkap', 101, '$alamat', '$email', '$tanggal_lahir')");
-
-    $conn_string = "host=localhost port=5432 dbname=sepatu user=postgres password=admin";
-    $koneksi = pg_connect($conn_string);
-
     $user_status=101;
     // $insert = pg_query($koneksi, "INSERT INTO member VALUES('','$nama_lengkap',$user_status::INT,'$alamat','$email','$tanggal_lahir');");
-    $insert = pg_query($koneksi, "CALL create_member('$nama_lengkap','$user_status','$alamat','$email','$tanggal_lahir');");
-    $select_member = pg_query($koneksi, "SELECT * FROM member WHERE nama='$nama_lengkap' AND alamat='$alamat' AND email='$email'");
-    $member = pg_fetch_array($select_member);
-    $member_id =$member[0];
+    $insert = mysqli_query($koneksi, "CALL create_member('$nama_lengkap','$user_status','$alamat','$email','$tanggal_lahir');");
+    $select_member = mysqli_query($koneksi, "SELECT * FROM member WHERE nama='$nama_lengkap' AND alamat='$alamat' AND email='$email'");
+    $member = mysqli_fetch_array($select_member);
+    $member_id = $member[0];
     echo $member_id;
-    $insert_login = pg_query($koneksi, "CALL create_login('$username','$password','$member_id','FALSE')");
+    $insert_login = mysqli_query($koneksi, "CALL create_login('$username','$password','$member_id','FALSE')");
     echo $insert_login;
 
     // note
