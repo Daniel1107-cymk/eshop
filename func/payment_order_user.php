0d074d679e0bf93b2f0c61a9bc7e7507973151dd
Jons221 <jonsusanto42@gmail.com>
2020-05-28 12:59:41 +0700

    update location

--- func/payment_order_user.php
+++ func/payment_order_user.php
@@ -59,7 +59,7 @@
         $history = "INSERT histori_trans VALUES (NULL,'','$id_last_resi','$member')";
         $result_history = mysqli_query($koneksi, $history);
     }
-    // header('location:../admin/admin_page.php?page=edit_order_list');
+    header('location:../admin/admin_page.php?page=resi_user');
 
 
     
